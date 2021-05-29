37eeb0661ef69c92493887f5346f59891c376959
Matt <git@mattthenub.dev>
2021-05-29 07:15:26 -0400

    Optimizations and clearer code (thanks clippy)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn pkg_count(pkgs: &mut usize, commands: &[&str], args: &[&str]) {
 			Ok(output) => String::from_utf8(output.stdout).unwrap(),
 			_ => "".to_string(),
 		};
-		*pkgs += list.split("\n").collect::<Vec<&str>>().len() - 1;
+		*pkgs += list.split('\n').count() - 1;
 	}
 }
 
@@ -18,43 +18,37 @@ fn main() {
 	let os_release = std::fs::read_to_string("/etc/os-release").unwrap();
 
 	for line in os_release.lines() {
-		if line.starts_with("NAME=") && config::OS != "" {
+		if line.starts_with("NAME=") && !config::OS.is_empty() {
 			print!("{}", config::OS);
 			println!("{} {}", &line[6..line.len() - 1], std::env::consts::ARCH);
 		}
 	}
 
-	if config::UPTIME != "" {
+	if !config::UPTIME.is_empty() {
 		let uptime = std::fs::read_to_string("/proc/uptime").unwrap();
-		let uptime: u64 = uptime.split(".").collect::<Vec<&str>>()[0].parse().unwrap();
+		let uptime: u64 = uptime.split('.').collect::<Vec<&str>>()[0].parse().unwrap();
 		print!("{}", config::UPTIME);
 		println!("{}h {}m", uptime / 60 / 60, uptime / 60 % 60);
 	}
 
-	if config::KERNEL != "" {
+	if !config::KERNEL.is_empty() {
 		let kernel_file = std::fs::read_to_string("/proc/version").unwrap();
-		let kernel = kernel_file.split(" ").collect::<Vec<&str>>()[2];
+		let kernel = kernel_file.split(' ').collect::<Vec<&str>>()[2];
 		println!("{}{}", config::KERNEL, kernel);
 	}
 
 	let shell_name = std::env::var("SHELL");
-	match (shell_name, config::SHELL != "") {
-		(Ok(shell_name), true) => {
-			let shell_vec = shell_name.split("/").collect::<Vec<&str>>();
-			println!("{}{}", config::SHELL, shell_vec.last().unwrap());
-		}
-		_ => (),
+	if let (Ok(shell_name), false) = (shell_name, config::SHELL.is_empty()) {
+		let shell_vec = shell_name.split('/').collect::<Vec<&str>>();
+		println!("{}{}", config::SHELL, shell_vec.last().unwrap());
 	}
 	let desktop = std::env::var("DESKTOP_SESSION");
-	match (desktop, config::DE != "") {
-		(Ok(desktop), true) => {
-			let desktop_vec = desktop.split("/").collect::<Vec<&str>>();
-			println!("{}{}", config::DE, desktop_vec.last().unwrap());
-		}
-		_ => (),
+	if let (Ok(desktop), false) = (desktop, config::DE.is_empty()) {
+		let desktop_vec = desktop.split('/').collect::<Vec<&str>>();
+		println!("{}{}", config::DE, desktop_vec.last().unwrap());
 	}
 
-	if config::PACKAGES != "" {
+	if !config::PACKAGES.is_empty() {
 		let mut pkgs = 0;
 		pkg_count(&mut pkgs, &["xbps-query"], &["-l"]);
 		pkg_count(&mut pkgs, &["flatpak", "apt", "kiss"], &["list"]);
@@ -77,5 +71,5 @@ fn main() {
 		base += 60;
 	}
 
-	println!("\x1b[38;2;128;128;128msfetch   v1.2.0");
+	println!("\x1b[38;2;128;128;128msfetch   v1.2.1");
 }
